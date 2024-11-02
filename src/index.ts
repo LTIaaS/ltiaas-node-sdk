81339d4964df7812dbf8f96c3b6ffe595e3914ba
Cvm <cvmcosta@ltiaas.com>
2024-11-02 17:20:10 -0300

    Exporting types

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,15 @@ export * from '@client/launch'
 export * from '@resources/idtoken/enums'
 export * from '@resources/deepLinking/enums'
 export * from '@resources/memberships/enums'
+export * from '@resources/platforms/enums'
 
 export type * from '@client/types'
 export type * from '@resources/idtoken/types'
 export type * from '@resources/deepLinking/types'
 export type * from '@resources/memberships/types'
 export type * from '@resources/lineitems/types'
+export type * from '@resources/scores/types'
+export type * from '@resources/registrations/types'
+export type * from '@resources/platforms/types'
 
 export default LTIAASLaunch
